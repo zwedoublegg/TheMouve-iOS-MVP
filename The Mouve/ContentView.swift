7179ee7e550aeab9a44ef888776044f30056f3fc
Samuel Ojogbo <ojogbo2@illinois.edu>
2024-02-16 23:06:48 -0500

    Initial Commit

---
+++ The Mouve/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Mouve
+//
+//  Created by Samuel Ojogbo on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Mouve/The_MouveApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_MouveApp.swift
+//  The Mouve
+//
+//  Created by Samuel Ojogbo on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct The_MouveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
