2e8b54c3cc46f17be1f44ecf43ce5b94f1d240bc
Samuel Ojogbo <ojogbo2@illinois.edu>
2024-04-02 09:57:42 -0400

    Added Firebase Cloud functions
    Added Firebase cloud functions for notifications

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+const functions = require("firebase-functions")
+const logger = require("firebase-functions/logger");
+
+admin.initializeApp();
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.sendNotification = functions.firestore.document('/messages/{uid}/{chatPartnerId}/{messageId}').onCreate((sanpshot, context) => {
+    const message = sanpshot.data();
+    logger.log(message)
+})
